ac701b6b6517acd7c1994775ac205b64a39932e9
Byron Sommardahl <byron@sommardahl.com>
2022-05-30 14:05:02 -0500

    getting all 4 calendars

--- scripts/calendar-selector.js
+++ scripts/calendar-selector.js
@@ -1,23 +1,32 @@
 
-function getUrls(contact) {
+function getUrls(level, isCommunity) {
 
-    console.log(contact);
-    return {
-        evalCalUrl: `https://api.logro.io/widget/appointment/service/badge-level-${contact.currentLevelStudying}?group=codex-evals`
+    function buildCalUrl(cal, level){
+        return `https://api.logro.io/widget/appointment/service/${cal}${level}?group=codex-evals`;
     }
+
+    if(isCommunity) return {
+        evalCalUrl: buildCalUrl("community-mastery", level),
+        capstoneCalUrl: buildCalUrl("community-capstone", level)
+    }
+    
+    return {
+        evalCalUrl: buildCalUrl("badge-level-", level),
+        capstoneCalUrl: buildCalUrl("capstone", level)
+    };
 }
 
 function loadContact(keyValStore) {
     var udJson = keyValStore.getItem("_ud");
     var contact =JSON.parse(udJson) || {};
 
-    if(contact.currentLevelStudying===undefined){
+    if(contact.currently_studying_level===undefined){
         console.log("No current level found");
         return {};
     }
 
     return {
         ...contact,
-        ...getUrls(contact)
+        ...getUrls(contact.currently_studying_level)
     }
 }
