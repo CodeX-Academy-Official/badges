1e4d148365fee501df2cf6aeec65e5ed0fdfb7b4
Byron Sommardahl <byron@sommardahl.com>
2022-05-30 13:42:06 -0500

    Experimenting

--- scripts/calendar-selector.js
+++ scripts/calendar-selector.js
@@ -1,15 +1,20 @@
-//"https://api.logro.io/widget/appointment/service/badge-level-1?group=codex-evals"
 
 function getUrls(contact) {
 
+    console.log(contact);
     return {
-        evalCalUrl: "https://api.logro.io/widget/appointment/service/badge-level-1?group=codex-evals"
+        evalCalUrl: `https://api.logro.io/widget/appointment/service/badge-level-${contact.currentLevelStudying}?group=codex-evals`
     }
 }
 
 function loadContact(keyValStore) {
     var udJson = keyValStore.getItem("_ud");
     var contact =JSON.parse(udJson) || {};
+
+    if(contact.currentLevelStudying===undefined){
+        console.log("No current level found");
+        return;
+    }
 
     return {
         ...contact,
